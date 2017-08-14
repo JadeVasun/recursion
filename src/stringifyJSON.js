ccd43e69d4f94366bd67c3db0908adaeddf067f1
Hack Reactor Student <student@hackreactor.com>
2017-08-14 15:31:48 -0700

    stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  if (obj === null || obj === undefined) {
+    return 'null';
+  }
+
+
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  //if array
+  if (Array.isArray(obj) === true) {
+    var results = [];
+
+    for (var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+      results.join();
+    }
+    return '[' + results + ']';
+  }
+
+  if (obj.constructor === Object) {
+    var results = [];
+    
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined ) {
+        return '{}';
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      results.join();
+    }
+    return '{' + results + '}';
+  }
+
+  return obj.toString();
+
 };
