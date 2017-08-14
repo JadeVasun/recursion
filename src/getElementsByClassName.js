10ef2e20f844136ebc29368d1c2ade9300a10b5d
Hack Reactor Student <student@hackreactor.com>
2017-08-14 12:25:21 -0700

    completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
   // your code here
+  if (node === undefined) {
+    node = document.body;
+  } else {
+    node = node;
+  }
+  
+  var results = [];
+  if (node.className.split(' ').indexOf(className) !== -1) {
+     results.push(node); 
+  }
+  if (node.children) {
+    for (var i = 0; i < node.children.length; i++) {
+      results = results.concat(getElementsByClassName(className, node.children[i]));
+    }
+  }
+
+  return results;
 };
